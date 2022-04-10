ff3e6f90c78ededab313c6ab3e6dfb3c86ca9942
pavleticdomen <94924836+pavleticdomen@users.noreply.github.com>
2022-04-10 14:45:25 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 # Nastavljeno za šolski strežnik
 $dbhost = "localhost";
 $dbuser = "udelezenec115";
-$dbpassword = "Ciklama+Ciklon16Amor";
+$dbpassword = "...";
 $dbname = "udelezenec115";
 
 if (!$connection = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname)) {
